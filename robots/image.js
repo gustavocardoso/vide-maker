ad7bb61f943bd3c7d03c0521caad969f55547779
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-04-13 11:44:37 -0700

    feat: add image download with fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state')
@@ -8,6 +9,7 @@ async function robot () {
   const content = state.load()
 
   await fetchImagesOfAllSentences(content)
+  await downloadAllImages(content)
 
   state.save(content)
 }
@@ -16,7 +18,7 @@ async function fetchGoogleAndReturnImagesLink (query) {
   const response = await customSearch.cse.list({
     auth: googleSearchCredentials.apiKey,
     cx: googleSearchCredentials.searchEngineId,
-    q: 'Michael Jackson',
+    q: query,
     searchType: 'image',
     num: 2
   })
@@ -31,10 +33,45 @@ async function fetchGoogleAndReturnImagesLink (query) {
 async function fetchImagesOfAllSentences (content) {
   for (const sentence of content.sentences) {
     const query = `${content.searchTerm} ${sentence.keywords[0]}`
-
     sentence.images = await fetchGoogleAndReturnImagesLink(query)
+
     sentence.googleSearchQuery = query
   }
+}
+
+async function downloadAllImages (content) {
+  content.downloadedImages = []
+
+  for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+    const images = content.sentences[sentenceIndex].images
+
+    for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+      const imageUrl = images[imageIndex]
+
+      try {
+        if (content.downloadedImages.includes(imageUrl)) {
+          throw new Error('Imagem já foi baixada')
+        }
+        await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+        content.downloadedImages.push(imageUrl)
+        console.log(
+          `> [${sentenceIndex}] [${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`
+        )
+        break
+      } catch (error) {
+        console.log(
+          `> [${sentenceIndex}] [${imageIndex}]  Erro ao baixar ${imageUrl}: ${error}`
+        )
+      }
+    }
+  }
+}
+
+async function downloadAndSave (url, fileName) {
+  return imageDownloader({
+    url,
+    dest: `./content/${fileName}`
+  })
 }
 
 module.exports = robot
